39373d6bc5488fd4bc9700b2e31444acb30bb96c
Ivo Rodrigues <14186480+IvoRodc@users.noreply.github.com>
2020-01-02 18:04:51 +0000

    Adicionada lista de postos para qualquer utilizador

--- e-Recarga/Controllers/HomeController.cs
+++ e-Recarga/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using e_Recarga.DAL;
+using e_Recarga.DTOs;
+using e_Recarga.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +11,7 @@ namespace e_Recarga.Controllers
 {
     public class HomeController : Controller
     {
+        private eRecargaDB db = new eRecargaDB();
         public ActionResult Index()
         {
             return View();
@@ -18,6 +22,26 @@ namespace e_Recarga.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        public ActionResult ListaPostos()
+        {
+            List<PostoCarregamentoDTO> lista = (from a in db.Postos
+                                where a.Ativo == true
+                                select new PostoCarregamentoDTO
+                                {
+                                    IDPosto = a.Id_PostoCarregamento,
+                                    Nome = a.Nome,
+                                    Potencia = a.VelocidadeCarregamento,
+                                    NTomadas = a.NumTomadas,
+                                    Municipio = a.Municipio,
+                                    Localizacao = a.Localizacao,
+                                    ValorInicial = a.ValorFixoInicial,
+                                    Menos30Min = a.ValorVariavelTempoMenos30Min,
+                                    Mais30Min = a.ValorVariavelTempoMais30Min
+                                })
+                            .ToList();
+            return View(lista);
         }
 
         public ActionResult Contact()
---
+++ e-Recarga/DTOs/PostoCarregamentoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_Recarga.DTOs
+{
+    public class PostoCarregamentoDTO
+    {
+        public int IDPosto { get; set; }
+        public string Nome { get; set; }
+        public double Potencia { get; set; }
+        public int NTomadas { get; set; }
+        public string Municipio { get; set; }
+        public string Localizacao { get; set; }
+        public double ValorInicial { get; set; }
+        public double Menos30Min { get; set; }
+        public double Mais30Min { get; set; }
+    }
+}
