5c7f6ff1a0c65aabc62b96fb752344b053010786
Ivo Rodrigues <14186480+IvoRodc@users.noreply.github.com>
2020-01-03 18:21:01 +0000

    Correção sufixos valores

--- e-Recarga/DTOs/PostoCarregamentoDTO.cs
+++ e-Recarga/DTOs/PostoCarregamentoDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,12 +10,20 @@ namespace e_Recarga.DTOs
     {
         public int IDPosto { get; set; }
         public string Nome { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F} kW", ApplyFormatInEditMode = false)]
         public double Potencia { get; set; }
         public int NTomadas { get; set; }
         public string Municipio { get; set; }
         public string Localizacao { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public double ValorInicial { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public double Menos30Min { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public double Mais30Min { get; set; }
     }
 }
