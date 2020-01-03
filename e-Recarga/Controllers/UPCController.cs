1dae68da02f49fa6def0b01d2151d229a6f8c0a6
Ivo Rodrigues <14186480+IvoRodc@users.noreply.github.com>
2020-01-03 20:01:29 +0000

    Inicio UPC Controler

---
+++ e-Recarga/Controllers/UPCController.cs
@@ -0,0 +1,40 @@
+﻿using e_Recarga.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Recarga.Controllers
+{
+    [Authorize(Roles = "UPC")]
+    public class UPCController : Controller
+    {
+        private eRecargaDB db = new eRecargaDB();
+        public ActionResult Index()
+        {
+            List<PostoCarregamento> lista = db.Postos.Where(p => p.Ativo == true).ToList();
+            return View(lista);
+        }
+
+        public ActionResult Detalhes(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PostoCarregamento postoCarregamento = db.Postos.Find(id);
+            if(postoCarregamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(postoCarregamento);
+        }
+
+        public ActionResult Reservar()
+        {
+            return View();
+        }
+    }
+}
---
+++ e-Recarga/DTOs/ReservaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace e_Recarga.DTOs
+{
+    public class ReservaDTO
+    {
+        public string IDUser { get; set; }
+        public int IDPosto { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+    }
+}
